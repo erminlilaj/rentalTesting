88cd50bcd869135177f3f36da20d22b459514368
jxel1 <jbitri22@epoka.edu.al>
2024-12-28 16:52:10 +0100

    Unused imports & compiler issues

--- src/main/java/it/linksmt/rental/dto/CreateReservationRequest.java
+++ src/main/java/it/linksmt/rental/dto/CreateReservationRequest.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
